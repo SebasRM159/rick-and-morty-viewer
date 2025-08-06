30731fc12105905e11f3e6a37f1dd00523dca9b8
Rami.com <94631744+SebasRamirezM@users.noreply.github.com>
2025-08-06 13:44:44 -0500

    New style to css

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,16 @@
 const Card = ({ character }) => {
     const [score, setScore] = useState(0);
-  return (<div>
-    <img src={character.image} alt={character.name} />
-    <h3>{character.name}</h3>
-    <p>Status: {character.status}</p>
-    <button onClick={() => setScore(score + 1)}>Like</button>
-    <button onClick={() => setScore(score - 1)}>Dislike</button>
-    <p>Puntaje: {score}</p>
+  return (
+  <div className="character-card">
+    <img src={character.image} alt={character.name} className="character-image"/>
+    <div className="character-info">
+        <h3 className="character-name">{character.name}</h3>
+        <p className={`character-status status-${character.status.toLowerCase()}`} >Status: {character.status}</p>
+        <p>Species: {character.species}</p>
+        <button className="vote-button like-button" onClick={() => setScore(score + 1)}>Like</button>
+        <button  className="vote-button dislike-button" onClick={() => setScore(score - 1)}>Dislike</button>
+        <p className="score">Puntaje: {score}</p>
+    </div>    
   </div>
   ) 
   
